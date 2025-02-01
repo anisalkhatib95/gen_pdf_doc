f07468ad4866b993f56921d9273b727968964634
Anis Alkhatib <alkhatib.anis@gmail.com>
2025-02-01 19:20:15 +0300

    add pages topic header and footer

---
+++ main.py
@@ -0,0 +1,32 @@
+from fpdf import FPDF
+import pandas
+
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
+
+df = pandas.read_csv("topics.csv", sep=",")
+
+for index, row in df.iterrows():
+        pdf.add_page()
+        pdf.set_font(family="Times", style="B", size=24)
+        pdf.set_text_color(100, 100, 100)
+        pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+        pdf.line(10, 21, 200, 21)
+        
+        # set footer
+        pdf.ln(265)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+        for _ in range(row["Pages"] - 1):
+            pdf.add_page()
+            
+            # set footer
+            pdf.ln(277)
+            pdf.set_font(family="Times", style="I", size=8)
+            pdf.set_text_color(180, 180, 180)
+            pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+
+pdf.output("output.pdf")
